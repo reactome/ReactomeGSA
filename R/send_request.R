ca6810c876f859e2fc25f79fc407b9b1c059de73
Johannes Griss <jgriss8@yahoo.com>
2020-02-19 19:36:58 +0100

    Use gzip to compress requests

--- R/send_request.R
+++ R/send_request.R
@@ -18,17 +18,24 @@ start_reactome_analysis <- function(request, reactome_url = NULL) {
 
   # create the json request
   json_request <- toJSON(request)
+  
+  # compress the JSON string
+  json_request_compress <- memCompress(from = json_request, type = "gzip")
 
   # send the request
-  request <- httr::POST(paste0(reactome_url, "0.1/analysis"), body = json_request,
-                        httr::content_type_json())
+  request <- httr::POST(paste0(reactome_url, "0.1/analysis"), body = json_request_compress,
+                        httr::content_type("application/gzip"), httr::add_headers("Content-Encoding" = "gzip"))
 
   # test if the request worked
   if (httr::status_code(request) != 200) {
-    as.character(httr::content(request, "text"))
-
-    error_obj <- jsonlite::fromJSON(httr::content(request, "text"))
-    stop("Request failed (", error_obj[["status"]], " - ", error_obj[["title"]], "): ", error_obj[["detail"]])
+    return_content <- httr::content(request, "text")
+    
+    if (substr(return_content, 1, 1) == "{") {
+      error_obj <- jsonlite::fromJSON(return_content)
+      stop("Request failed (", error_obj[["status"]], " - ", error_obj[["title"]], "): ", error_obj[["detail"]])
+    } else {
+      stop("Failed to submit analysis request: ", return_content)
+    }  
   }
 
   analysis_id = httr::content(request, "text")
