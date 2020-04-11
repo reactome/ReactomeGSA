8cad636ca1dd66d42fdc8c1db6832285364ba786
Johannes Griss <jgriss8@yahoo.com>
2020-04-11 08:40:01 +0200

    Added function analyse_sc_clusters
    Moved to version 1.1.2
    Added multiple functions to plot GSVA results and analyse scRNA-seq data

---
+++ R/analyse_sc_clusters.R
@@ -0,0 +1,165 @@
+
+# analyse_sc_clusters methods ----
+
+
+#' analyse_sc_clusters
+#' 
+#' Analyses cell clusters of a single-cell RNA-sequencing
+#' experiment to get pathway-level expressions for every
+#' cluster of cells.
+#' 
+#' There are currently two specific implementations of
+#' this function, one to support \code{Seurat} objects
+#' and one to support Bioconductor's \code{SingleCellExperiment}
+#' class.
+#'
+#' @param object The object containing the single-cell RNA-sequencing data.
+#' @param use_interactors If set (default), protein-protein interactors from IntAct are used to
+#'                            extend Reactome pathways.
+#' @param include_disease_pathways If set, disease pathways are included as well. Disease pathways in
+#'                                 Reactome follow a different annotation approach and can therefore
+#'                                 lead to inaccurate results.
+#' @param create_reactome_visualization If set, the interactive visualization in Reactome's PathwayBrowser
+#'                                      is created.
+#' @param create_reports If set, PDF and Microsoft Excel reports are created. Links to these report files
+#'                       are send to the supplied e-mail address.
+#' @param report_email The e-mail address to which reports should be sent to.
+#' @param verbose If set, additional status messages are printed.
+#' @param ... Parameters passed to the specific implementation. Detailed documentations
+#'            can be found there.
+#'
+#' @return A \code{\link{ReactomeAnalysisResult}} object.
+#' @export
+#'
+#' @examples
+#' # This example shows how a Seurat object can be analysed
+#' # the approach is identical for SingleCellExperiment objects
+#' library(ReactomeGSA.data)
+#' data(jerby_b_cells)
+#' 
+#' # perform the GSVA analysis
+#' gsva_result <- analyse_sc_clusters(jerby_b_cells, verbose = FALSE)
+setGeneric("analyse_sc_clusters", function(object, use_interactors = TRUE, 
+                                           include_disease_pathways = FALSE,  
+                                           create_reactome_visualization = FALSE,
+                                           create_reports = FALSE,
+                                           report_email = NULL,
+                                           verbose = FALSE, ...) standardGeneric("analyse_sc_clusters"))
+
+#' analyse_sc_clusters - Seurat
+#'
+#' @inherit analyse_sc_clusters
+#' 
+#' @param object The \code{Seurat} object containing the single cell RNA-sequencing data.
+#' @param ... Parameters passed to Seurat's \code{AverageExpression} function.
+setMethod("analyse_sc_clusters", c("object" = "Seurat"), function(object, use_interactors = TRUE, 
+                                                                  include_disease_pathways = FALSE,  
+                                                                  create_reactome_visualization = FALSE,
+                                                                  create_reports = FALSE,
+                                                                  report_email = NULL,
+                                                                  verbose = FALSE, ...) {
+  # create the parameters for the AverageExpression call
+  if (verbose) message("Converting parameters...")
+  seurat_params <- list(...)
+  seurat_params["object"] = object
+  seurat_params["verbose"] = verbose
+  
+  # get the count data
+  if (verbose) message("Calculating average expression per cluster...")
+  counts <- do.call(Seurat::AverageExpression, seurat_params)
+  
+  # only use default assay
+  if (is(counts, "list")) {
+    default_assay <- Seurat::DefaultAssay(object)
+    
+    if (!default_assay %in% names(counts)) {
+      stop("Error: Failed to retrieve average expression for default assay '", default_assay, "'")
+    }
+    
+    counts <- counts[[default_assay]]
+  }
+  
+  # create the ReactomeGSA request
+  request <- ReactomeGSA::ReactomeAnalysisRequest(method = "ssGSEA")
+  
+  # set the default request parameters
+  request <- ReactomeGSA::set_parameters(request, use_interactors = use_interactors, 
+                                         include_disease_pathways = include_disease_pathways, 
+                                         create_reactome_visualization = create_reactome_visualization,
+                                         create_reports = create_reports)
+  
+  if (!is.null(report_email)) {
+    request <- ReactomeGSA::set_parameters(request, email = report_email)
+  }
+  
+  # create the request object
+  df_counts <- data.frame(counts)
+  cell_groups <- colnames(df_counts)
+  
+  request <- ReactomeGSA::add_dataset(request, expression_values = df_counts, 
+                                      name = "Seurat", type = "rnaseq_counts", 
+                                      comparison_factor = "Cluster", comparison_group_1 = unique(cell_groups)[1], comparison_group_2 = unique(cell_groups)[2], 
+                                      sample_data = data.frame(row.names = cell_groups, Cluster = cell_groups))
+  
+  # run the analysis
+  gsa_res <- ReactomeGSA::perform_reactome_analysis(request, verbose = verbose)
+  
+  return(gsa_res)
+})
+
+#' analyse_sc_clusters - SingleCellExperiment
+#'
+#' @param cell_id The metadata field to use to group cells (for example "cluster")
+#' @inherit analyse_sc_clusters
+#' 
+#' @param object The \code{SingleCellExperiment} object containing the single cell RNA-sequencing data.
+#' @param ... Parameters passed to scater's \code{aggregateAcrossCells} function.
+setMethod("analyse_sc_clusters", c("object" = "SingleCellExperiment"), function(object, use_interactors = TRUE, 
+                                                                  include_disease_pathways = FALSE,  
+                                                                  create_reactome_visualization = FALSE,
+                                                                  create_reports = FALSE,
+                                                                  report_email = NULL,
+                                                                  verbose = FALSE,
+                                                                  cell_id, ...) {
+  # make sure scater is available
+  if (!requireNamespace("scater")) {
+    stop("Error: This function requires 'scater'. Please install it using BiocManager::install(\"scater\")")
+  }
+  
+  # create the parameters for the AverageExpression call
+  scater_params <- list(...)
+  scater_params["x"] <- object
+  scater_params["ids"] = cell_id
+  
+  # get the count data
+  if (verbose) message("Calculating average expression per cluster...")
+  agg_counts <- do.call(scater::aggregateAccrossCells, scater_params)
+  counts <- SingleCellExperiment::counts(agg_counts)
+  
+  # create the ReactomeGSA request
+  request <- ReactomeGSA::ReactomeAnalysisRequest(method = "ssGSEA")
+  
+  # set the default request parameters
+  request <- ReactomeGSA::set_parameters(request, use_interactors = use_interactors, 
+                                         include_disease_pathways = include_disease_pathways, 
+                                         create_reactome_visualization = create_reactome_visualization,
+                                         create_reports = create_reports)
+  
+  if (!is.null(report_email)) {
+    request <- ReactomeGSA::set_parameters(request, email = report_email)
+  }
+  
+  # create the request object
+  df_counts <- data.frame(counts)
+  cell_groups <- colnames(df_counts)
+  
+  request <- ReactomeGSA::add_dataset(request, expression_values = df_counts, 
+                                      name = "Seurat", type = "rnaseq_counts", 
+                                      comparison_factor = "Cluster", comparison_group_1 = unique(cell_groups)[1], comparison_group_2 = unique(cell_groups)[2], 
+                                      sample_data = data.frame(row.names = cell_groups, Cluster = cell_groups))
+  
+  # run the analysis
+  gsa_res <- ReactomeGSA::perform_reactome_analysis(request, verbose = verbose)
+  
+  return(gsa_res)
+})
---
+++ R/gsva_plotting_functions.R
@@ -0,0 +1,289 @@
+#' plot_gsva_pathway
+#' 
+#' Plots the expression of a specific pathway from a ssGSEA result.
+#'
+#' @param object The \code{\link{ReactomeAnalysisResult}} object.
+#' @param pathway_id The pathway's id
+#' @param ... Additional parameters for specific implementations.
+#'
+#' @return A ggplot2 plot object
+#' @export
+#' @importFrom ggplot2 ggplot aes geom_point geom_hline labs
+#' @family ReactomeAnalysisResult functions
+#'
+#' @examples
+#' # load the scRNA-seq example data
+#' library(ReactomeGSA.data)
+#' data(jerby_b_cells)
+#' 
+#' # perform the GSVA analysis
+#' gsva_result <- analyse_sc_clusters(jerby_b_cells, verbose = FALSE)
+#' 
+#' # create the plot
+#' plot_obj <- plot_gsva_pathway(gsva_result, "R-HSA-389542")
+setGeneric("plot_gsva_pathway", function(object, pathway_id, ...) callGeneric("plot_gsva_pathway"))
+
+#' ReactomeAnalysisResult - plot_gsva_pathway
+#'
+#' @inherit plot_gsva_pathway
+setMethod("plot_gsva_pathway", c("object" = "ReactomeAnalysisResult"), function(object, pathway_id, ...)  {
+  # make sure it is a GSVA result
+  if (!is_gsva_result(object)) {
+    stop("ReactomeAnalysisResult object does not represent a GSVA result (choose method 'ssGSEA' during the analysis)")
+  }
+  
+  gsa_pathways <- pathways(object)
+  
+  # get the pathway
+  if (!pathway_id %in% rownames(gsa_pathways)) {
+    stop("Error: Unknown pathway ", pathway_id)
+  }
+  
+  pathway_name <- gsa_pathways[pathway_id, "Name"]
+  
+  # remove the name
+  gsa_pathways$Name <- NULL
+  
+  # fix the column names
+  colnames(gsa_pathways) <- gsub("\\.[^.]*$", "", colnames(gsa_pathways))
+  colnames(gsa_pathways) <- gsub("\\.", " ", colnames(gsa_pathways))
+  
+  # get the data.frame for ggplot
+  plot_data <- data.frame(
+    cluster_id = colnames(gsa_pathways),
+    expr = as.numeric(gsa_pathways[pathway_id, ])
+  )
+  
+  sorted_ids <- plot_data$cluster_id[order(plot_data$expr)]
+  plot_data$cluster_id <- factor(plot_data$cluster_id, levels = sorted_ids)
+  
+  plot_obj <- ggplot2::ggplot(plot_data, ggplot2::aes(x = cluster_id, y = expr)) + 
+    ggplot2::geom_bar(stat = "identity") +
+    ggplot2::labs(x = "Cluster", y = "Pathway expression", title = paste0(pathway_name, "(", pathway_id, ")")) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
+  
+  return(plot_obj)
+})
+
+# plot_gsva_heatmap functions ----
+
+#' plot_gsva_heatmap
+#' 
+#' Plots pathway expression values / sample as a heatmap. Ranks pathways based on their
+#' expression difference.
+#'
+#' @param object The \code{\link{ReactomeAnalysisResult}} object.
+#' @param pathway_ids A vector of pathway ids. If set, only these pathways are included in the plot.
+#' @param max_pathways The maximum number of pathways to include. Only takes effect if \code{pathway_ids}
+#'                     is not set.
+#' @param truncate_names If set, long pathway names are truncated.
+#' @param ... Additional parameters passed to specific implementations.
+#'
+#' @return None
+#' @family ReactomeAnalysisResult functions
+#' @export
+#'
+#' @examples
+#' # load the scRNA-seq example data
+#' library(ReactomeGSA.data)
+#' data(jerby_b_cells)
+#' 
+#' # perform the GSVA analysis
+#' gsva_result <- analyse_sc_clusters(jerby_b_cells, verbose = FALSE)
+#' 
+#' # plot the heatmap
+#' relevant_pathways <- c("R-HSA-983170", "R-HSA-388841", "R-HSA-2132295", "R-HSA-983705", "R-HSA-5690714")
+#' plot_gsva_heatmap(gsva_result, 
+#'                   pathway_ids = relevant_pathways, # limit to these pathways
+#'                   margins = c(6,30), # adapt the figure margins in heatmap.2
+#'                   dendrogram = "col", # only plot column dendrogram
+#'                   scale = "row", # scale for each pathway
+#'                   key = FALSE, # don't display the color key
+#'                   lwid=c(0.1,4)) # remove the white space on the left
+setGeneric("plot_gsva_heatmap", function(object, pathway_ids = NULL, max_pathways = 20, truncate_names = TRUE, ...) callGeneric("plot_gsva_heatmap"))
+
+#' plot_gsva_heatmap - ReactomeAnalysisResult function
+#' 
+#' @param ... Additional parameters passed to the heatmap.2 function.
+#' 
+#' @importFrom gplots heatmap.2
+#' @inherit plot_gsva_heatmap
+setMethod("plot_gsva_heatmap", c("object" = "ReactomeAnalysisResult"), function(object, pathway_ids = NULL, max_pathways = 20, truncate_names = TRUE, ...) {
+  # make sure it is a GSVA result
+  if (!is_gsva_result(object)) {
+    stop("ReactomeAnalysisResult object does not represent a GSVA result (choose method 'ssGSEA' during the analysis)")
+  }
+  
+  gsa_pathways <- pathways(object)
+  
+  # sort the pathways based on their difference
+  max_difference <- do.call(rbind, apply(gsa_pathways, 1, function(row) {
+    values <- as.numeric(row[2:length(row)])
+    return(data.frame(name = row[1], min = min(values), max = max(values)))
+  }))
+  
+  max_difference$diff <- max_difference$max - max_difference$min
+  
+  gsa_pathways <- gsa_pathways[order(max_difference$diff, decreasing = T), ]
+  
+  # get the pathways to plot
+  if (!is.null(pathway_ids)) {
+    # make sure all pathways exist
+    if (!all(pathway_ids %in% rownames(gsa_pathways))) {
+      warning("Warning: No results for the following pathways: ", paste(pathway_ids[!pathway_ids %in% rownames(gsa_pathways)], collapse = ", ", sep = ", "))
+      
+      pathway_ids <- pathway_ids[pathway_ids %in% rownames(gsa_pathways)]
+    }
+  } else {
+    # use the top different pathways
+    if (nrow(gsa_pathways) > max_pathways) {
+      pathway_ids <- rownames(gsa_pathways)[1:max_pathways]
+    } else {
+      pathway_ids <- rownames(gsa_pathways)
+    }
+  }
+  
+  # get the expression values as a matrix
+  expression_matrix <- as.matrix(gsa_pathways[pathway_ids, 2:ncol(gsa_pathways)])
+  
+  # remove the redundant dataset name
+  if (length(names(object)) == 1) {
+    colnames(expression_matrix) <- gsub("\\.[^\\.]*$", "", colnames(expression_matrix))
+  }
+  
+  # use pathway names
+  rownames(expression_matrix) <- gsa_pathways[rownames(expression_matrix), "Name"]
+  
+  # shorten the names
+  if (truncate_names) {
+    is_long <- nchar(rownames(expression_matrix)) > 40
+    rownames(expression_matrix)[is_long] <- paste0(substr(rownames(expression_matrix)[is_long], 1, 32), "...")
+  }
+  
+  # merge default with user parameters
+  heatmap_params <- list(
+    x = expression_matrix, 
+    margins = c(6, 15), 
+    density.info = "none", 
+    trace = "none",
+    col = RColorBrewer::brewer.pal(9, "RdYlBu"))
+  
+  user_params <- list(...)
+  for (param_name in names(user_params)) {
+    if (nchar(param_name) > 0) {
+      heatmap_params[[param_name]] <- user_params[[param_name]]
+    }
+  }
+  
+  # create the heatmap
+  do.call(gplots::heatmap.2, heatmap_params)
+})
+
+# plot_gsva_pca ----
+
+#' plot_gsva_pca
+#' 
+#' Runs a Principal Component analysis (using \code{prcomp}) on the samples
+#' based on the pathway analysis results.
+#'
+#' @param object A \code{\link{ReactomeAnalysisResult}} object containing a ssGSEA result
+#' @param pathway_ids A character vector of pathway ids. If set, only these pathways will be
+#'                    used for the PCA analysis.
+#' @param ... Additional paramters passed to specific implementations.
+#'
+#' @return A ggplot2 object representing the plot.
+#' @export
+#'
+#' @examples 
+#' # load the scRNA-seq example data
+#' library(ReactomeGSA.data)
+#' data(jerby_b_cells)
+#' 
+#' # perform the GSVA analysis
+#' gsva_result <- analyse_sc_clusters(jerby_b_cells, verbose = FALSE)
+setGeneric("plot_gsva_pca", function(object, pathway_ids = NULL, ...) callGeneric("plot_gsva_pca"))
+
+#' plot_gsva_pca - ReactomeAnalysisResult
+#' 
+#' @param ... Additional parameters are passed to \code{prcomp}
+#' 
+#' @inherit plot_gsva_pca
+setMethod("plot_gsva_pca", c("object" = "ReactomeAnalysisResult"), function(object, pathway_ids = NULL, ...) {
+  if (!is_gsva_result(object)) {
+    stop("ReactomeAnalysisResult object does not represent a GSVA result (choose method 'ssGSEA' during the analysis)")
+  }
+  
+  gsa_pathways <- pathways(object)
+  
+  # get the pathways to plot
+  if (!is.null(pathway_ids)) {
+    # make sure all pathways exist
+    if (!all(pathway_ids %in% rownames(gsa_pathways))) {
+      warning("Warning: No results for the following pathways: ", paste(pathway_ids[!pathway_ids %in% rownames(gsa_pathways)], collapse = ", ", sep = ", "))
+      
+      pathway_ids <- pathway_ids[pathway_ids %in% rownames(gsa_pathways)]
+    }
+  } else {
+    pathway_ids <- rownames(gsa_pathways)
+  }
+  
+  # get the expression values as a matrix
+  expression_matrix <- as.matrix(gsa_pathways[pathway_ids, 2:ncol(gsa_pathways)])
+  
+  # remove the redundant dataset name
+  if (length(names(gsva_result)) == 1) {
+    colnames(expression_matrix) <- gsub("\\.[^\\.]*$", "", colnames(expression_matrix))
+  }
+  
+  # perform the PCA
+  prcomp_params <- list(x = expression_matrix)
+  
+  user_params = list(...)
+  for (param_name in names(user_params)) {
+    if (nchar(param_name) > 0 ) {
+      prcomp_params[[param_name]] <- user_params[[param_name]]
+    }
+  }
+  
+  pca_fit <- do.call(stats::prcomp, prcomp_params)
+  pca_summary <- summary(pca_fit)
+  
+  # create the data for ggplot
+  plot_data <- data.frame(pca_fit$rotation)
+  plot_data$sample <- rownames(pca_fit$rotation)
+  
+  # get the proportion of variance
+  prop_var_x <- round(pca_summary$importance["Proportion of Variance", "PC1"] * 100, 1)
+  prop_var_y <- round(pca_summary$importance["Proportion of Variance", "PC2"] * 100, 1)
+  
+  plot_obj <- ggplot2::ggplot(plot_data, ggplot2::aes(x = PC1, y = PC2, color = sample)) +
+    ggplot2::geom_point(size = 3) +
+    ggplot2::labs(x = paste0("PC1 (", prop_var_x, "% var. expl.)"), y = paste0("PC2 (", prop_var_y, "% var. expl.)")) +
+    ggplot2::theme_bw()
+  
+  return(plot_obj)
+})
+
+#' is_gsva_result
+#'
+#' @param object A \code{\link{ReactomeAnalysisResult}} object
+#'
+#' @return Boolean indicating whether the object is a GSVA result.
+#'
+#' @examples
+is_gsva_result <- function(object) {
+  # must contain the "pathways" result
+  if (!"pathways" %in% ReactomeGSA::result_types(object)) {
+    return(FALSE)
+  }
+  
+  # must not contain the FDR column
+  for (dataset in names(object)) {
+    if ("FDR" %in% colnames(ReactomeGSA::get_result(object, "pathways", dataset))) {
+      return(FALSE)
+    }
+  }
+  
+  return(TRUE)
+}
